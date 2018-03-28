1c778ffd77e0c8804f6c86c2b6ac6f2c6cd262ad
IlonaArt <ilonasurdream@gmail.com>
2018-03-28 22:25:13 +0300

    Добавила iframe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
 var buttonSearch = document.querySelector(".button-search-hotels");
 var searchForm = document.querySelector(".search");
+var arrival = document.querySelector("[name=arrival]");
+var dates = document.querySelector(".dates-text");
+var numbers = document.querySelector(".members");
 
 buttonSearch.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	searchForm.classList.remove("visually-hidden");
+	searchForm.classList.toggle("search");
+	arrival.focus();
+});
+
+searchForm.addEventListener("submit", function (evt) {
+	if (!dates.value || !numbers.value) {
+	evt.preventDefault();
+	console.log("Введите необходимые данные")	
+	}
+});
+
+buttonSearch.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	searchForm.classList.toggle("search-show");
 });
